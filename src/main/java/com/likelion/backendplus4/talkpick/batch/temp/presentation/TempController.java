6c66d9be65fd03ca8135e8b7bd0015ada8286879
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-05-09 15:07:58 +0900

    ♻️ Refactor: HTTP 메서드 수정

--- src/main/java/com/likelion/backendplus4/talkpick/batch/temp/presentation/TempController.java
+++ src/main/java/com/likelion/backendplus4/talkpick/batch/temp/presentation/TempController.java
@@ -2,6 +2,7 @@ package com.likelion.backendplus4.talkpick.batch.temp.presentation;
 
 import com.likelion.backendplus4.talkpick.batch.temp.application.TempService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,12 @@ public class TempController {
     private final TempService tempService;
 
 
-    @RequestMapping("/fail-case1")
+    @GetMapping("/fail-case1")
     public String failCase1() {
         return tempService.failCase1();
     }
 
-    @RequestMapping("/fail-case2")
+    @GetMapping("/fail-case2")
     public String failCase2() {
         return tempService.failCase2(null);
     }

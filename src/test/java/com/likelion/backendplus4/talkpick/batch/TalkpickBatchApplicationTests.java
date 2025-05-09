60aa462ce9b29c40ee51aa954500630fd08b4799
Yejeong, Ham <dev@thelightway.kr>
2025-05-09 09:18:50 +0900

    📦 Chore: TalkPick 프로젝트 초기 설정 (#1)
    ### 의존성 추가
    * 스프링 부트 3.4.5
    * 롬복
    * MySql 드라이버
    * JPA
    * 엘라스틱 서치
    * Open-Ai (스프링 AI)
    * Swagger
    
    ### 이슈/PR템플릿 추가
    * 이슈 템플릿: config, epic, feature, tech-debt, user-story
    * PR 템플릿: 기본 템플릿, bugfix

---
+++ src/test/java/com/likelion/backendplus4/talkpick/batch/TalkpickBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.backendplus4.talkpick.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkpickBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

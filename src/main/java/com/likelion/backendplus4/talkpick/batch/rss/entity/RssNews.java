cc2d94d16960eb2140dc16c3d7cd98d3764c2a10
Atriel <fortuny1999@gmail.com>
2025-05-10 17:53:40 +0900

    🐛Fix: RssNews 엔티티에서 @Data 어노테이션을 세분화된 Lombok 어노테이션으로 대체
    - @Data 제거 후 @Getter로 전체 getter 생성
    - 필요한 필드에만 선택적으로 @Setter 적용하여 식별자 무결성 보호
    - @ToString에 description 필드 제외하여 로깅 최적화
    - @EqualsAndHashCode에 id 필드만 사용하여 객체 비교 최적화

--- src/main/java/com/likelion/backendplus4/talkpick/batch/rss/entity/RssNews.java
+++ src/main/java/com/likelion/backendplus4/talkpick/batch/rss/entity/RssNews.java
@@ -1,31 +1,43 @@
 package com.likelion.backendplus4.talkpick.batch.rss.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+
+/**
+ * RSS 피드를 수집 객체
+ *
+ * @author 양병학
+ * @since 2024-05-10 최초 작성
+ * @modify 2024-05-10 17:47 PR 수정
+ * @ToString exclude로 대량의 텍스트필드 로그에서 제외
+ * @Data -> @Getter후 Setter는 개별 지정해서 식별자 보호
+ * @EqualsAndHashCode 지정으로 갹채 비교 최적화
+ */
 @Entity
 @Table(name = "rss", uniqueConstraints = @UniqueConstraint(columnNames = {"link"}))
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString(exclude = "description")
+@EqualsAndHashCode(of = "id")
 public class RssNews {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     @Column(nullable = false)
     private String title;
 
     @Column(nullable = false, unique = true)
     private String link;
 
+    @Setter
     @Column(name = "pub_date")
     private LocalDateTime pubDate;
 
@@ -35,6 +47,7 @@ public class RssNews {
     @Column
     private String guid;
 
+    @Setter
     @Column(columnDefinition = "TEXT")
     private String description;
 

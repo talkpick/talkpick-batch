ad7f9d01e3c55ac55e58f65bfeef32fc45b96e3d
JUNG ANSIK <jaszzzzz@naver.com>
2025-05-14 10:01:15 +0900

    ✨  Feature: #54-뉴스-데이터-수집-포트-규격-개발 (#55)
    * ✨  Feature/#54-뉴스-데이터-수집-포트-규격-개발
    
    * ♻️ Refactor: Port 메서드 수정
    
    * ♻️ Refactor: 도메인 객체 내부 Category필드 추가

---
+++ src/main/java/com/likelion/backendplus4/talkpick/batch/index/domain/model/NewsInfo.java
@@ -0,0 +1,17 @@
+package com.likelion.backendplus4.talkpick.batch.index.domain.model;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class NewsInfo{
+	private final String newsId;
+	private final String title;
+	private final String content;
+	private final LocalDateTime publishedAt;
+	private final String imageUrl;
+	private final String category;
+	}

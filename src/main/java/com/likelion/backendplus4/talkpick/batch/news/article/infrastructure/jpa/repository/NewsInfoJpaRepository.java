29b7276a66cb66cc98804d65f71ff9b55008a6a2
thelightway <dev@thelightway.kr>
2025-05-14 10:50:26 +0900

    ♻️ Refactor: RssNewsRepository 이름 변경 -> NewsInfoJpaRepository

--- src/main/java/com/likelion/backendplus4/talkpick/batch/news/article/infrastructure/jpa/repository/NewsInfoJpaRepository.java
+++ src/main/java/com/likelion/backendplus4/talkpick/batch/news/article/infrastructure/jpa/repository/NewsInfoJpaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RssNewsRepository extends JpaRepository<ArticleEntity, Long> {
+public interface NewsInfoJpaRepository extends JpaRepository<ArticleEntity, Long> {
 
     boolean existsByLink(String link);
 }

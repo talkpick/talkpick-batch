a9d5a8609304c45235cc943ca4ee54d8cb1bf341
Yejeong, Ham <dev@thelightway.kr>
2025-05-18 22:01:09 +0900

    ✨ Feature: #76 색인 정보 제공 포트에 뉴스 요약 추가 (#77)
    * ✨ Feature: NewsInfo 도메인 내용 요약 및 벡터 필드 추가
    
    * ✨ Feature: 필드 추가에 따른 mapper 수정
    
    * 📦 Chore: 컴파일 오류 해결을 위한 샘플 코드 수정

--- src/main/java/com/likelion/backendplus4/talkpick/batch/index/domain/model/NewsInfo.java
+++ src/main/java/com/likelion/backendplus4/talkpick/batch/index/domain/model/NewsInfo.java
@@ -10,15 +10,18 @@ import lombok.RequiredArgsConstructor;
  * 뉴스 정보를 표현하는 도메인 모델
  *
  * @since 2025-05-15
+ * @modified 2025-05-18
  */
 @RequiredArgsConstructor
 @Getter
 @Builder
-public class NewsInfo{
+public class NewsInfo {
 	private final String newsId;
 	private final String title;
 	private final String content;
 	private final LocalDateTime publishedAt;
 	private final String imageUrl;
 	private final String category;
-	}
+	private final String summary;
+	private final float[] summaryVector;
+}

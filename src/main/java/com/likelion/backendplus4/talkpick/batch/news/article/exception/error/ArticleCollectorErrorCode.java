866162ec2fb2ba462527b141722b49325a9fed0d
Atriel <fortuny1999@gmail.com>
2025-05-25 17:55:03 +0900

    ✨ Feat: 에러코드 추가 및 관련 Exception 추가 (배치작업 noskip용 exception)

--- src/main/java/com/likelion/backendplus4/talkpick/batch/news/article/exception/error/ArticleCollectorErrorCode.java
+++ src/main/java/com/likelion/backendplus4/talkpick/batch/news/article/exception/error/ArticleCollectorErrorCode.java
@@ -52,7 +52,14 @@ public enum ArticleCollectorErrorCode implements ErrorCode {
 
     // 데이터베이스 관련 오류
     DB_SAVE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 530001, "RSS 뉴스를 저장하는 중 오류가 발생했습니다."),
-    DUPLICATE_LINK_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 450006,  "이미 존재하는 링크입니다.");
+    DUPLICATE_LINK_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 450006,  "이미 존재하는 링크입니다."),
+
+    // 조회수 동기화 관련 오류 (새로 추가)
+    VIEW_COUNT_INVALID_FORMAT(HttpStatus.BAD_REQUEST, 460001, "조회수 값의 형식이 올바르지 않습니다"),
+    VIEW_COUNT_UPDATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 460002, "조회수 업데이트 처리 중 오류가 발생했습니다"),
+    VIEW_COUNT_CLEANUP_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 460003, "오래된 조회수 데이터 정리 중 오류가 발생했습니다"),
+    VIEW_COUNT_SYNC_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 460004, "조회수 동기화 처리 중 오류가 발생했습니다"),
+    VIEW_COUNT_BATCH_UPDATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 460005, "조회수 배치 업데이트 중 오류가 발생했습니다");
 
     private final HttpStatus status;
     private final int code;

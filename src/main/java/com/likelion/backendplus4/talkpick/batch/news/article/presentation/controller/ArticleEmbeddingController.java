9029ba921a32f43b06e275bd7ca5814ec44e67b0
thelightway <dev@thelightway.kr>
2025-05-29 05:29:05 +0900

    📦 Chore: 주석 추가

--- src/main/java/com/likelion/backendplus4/talkpick/batch/news/article/presentation/controller/ArticleEmbeddingController.java
+++ src/main/java/com/likelion/backendplus4/talkpick/batch/news/article/presentation/controller/ArticleEmbeddingController.java
@@ -10,12 +10,27 @@ import com.likelion.backendplus4.talkpick.batch.news.article.application.port.ou
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * 뉴스 기사 정보를 임베딩하는 작업을 수행하는 REST 컨트롤러입니다.
+ *
+ * 이 컨트롤러는 클라이언트로부터 요청을 받아 NewsEmbedUseCase를 통해 <p>
+ * 데이터베이스에 저장된 모든 뉴스 기사의 내용을 임베딩 처리합니다. <p>
+ *
+ * @since 2025-05-29
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/news")
 public class ArticleEmbeddingController {
 	private final NewsEmbedUseCase newsEmbedUseCase;
 
+	/**
+	 * 전체 뉴스 정보를 임베딩하는 API
+	 *
+	 * @return 성공 여부를 담은 ApiResponse
+	 * @author 함예정
+	 * @since 2025-05-29
+	 */
 	@PostMapping("/embed")
 	public ResponseEntity<ApiResponse<Void>> indexAllNews() {
 		newsEmbedUseCase.embedAllNewsInfo();

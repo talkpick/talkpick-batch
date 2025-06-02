f76335f9e9dac8fd809322069dca0f37784ee801
Yejeong, Ham <dev@thelightway.kr>
2025-06-02 16:24:53 +0900

    🐛 Fix: CDN 캐시 오류 수정 (#100)

--- src/main/java/com/likelion/backendplus4/talkpick/batch/cache/application/port/out/CacheResetPort.java
+++ src/main/java/com/likelion/backendplus4/talkpick/batch/cache/application/port/out/CacheResetPort.java
@@ -3,7 +3,7 @@ package com.likelion.backendplus4.talkpick.batch.cache.application.port.out;
 import java.util.List;
 
 public interface CacheResetPort {
-	boolean purgePrefix(String prefix);
+	boolean purgePrefix(List<String> prefix);
 
 	boolean purgeFile(List<String> fileUrls);
 
